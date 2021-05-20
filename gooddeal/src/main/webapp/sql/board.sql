6545e0334663ce01e66a536986e2b59e1d94ec71
rlaeh <rlaeh@LAPTOP-1EISNQHF>
2021-05-20 18:26:04 +0900

    시퀀스 추가

--- gooddeal/src/main/webapp/sql/board.sql
+++ gooddeal/src/main/webapp/sql/board.sql
@@ -1,23 +1,32 @@
 create table board(
 	boa_num number not null primary key,
-	mem_num number foreign key,
+	mem_num number not null,
 	boa_title varchar2(150) not null,
 	boa_content clob not null,
 	boa_uploadfile blob,
 	boa_date date not null,
 	boa_update date not null,
-	boa_view number not null	
+	boa_view number not null,
+    CONSTRAINT board_fk foreign key (mem_num) references member (mem_num)
 );
 
 create table board_reply( 
 	bor_num number not null primary key,
-	boa_num number not null foreign key,
+	boa_num number not null,
 	bor_content varchar2(3000) not null,
-	bor_date blob not null	
+	bor_date blob not null,
+    CONSTRAINT board_fk2 foreign key (boa_num) references board (boa_num)
 );
            
 create table board_fav(
 	bof_num number not null primary key,
-	boa_num number not null foreign key,
-	mem_num number not null foreign key
+	boa_num number not null,
+	mem_num number not null,
+    CONSTRAINT board_fk3 foreign key (boa_num) references board (boa_num),
+    CONSTRAINT board_fk4 foreign key (mem_num) references member (mem_num)
 );
+
+create sequence board_seq;
+create sequence board_reply_seq;
+create sequence board_fav_seq;
+
