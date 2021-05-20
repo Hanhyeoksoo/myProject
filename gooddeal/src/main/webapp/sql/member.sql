d0367212429b4b55a8039b2b1109fd7c9b68e129
vds11 <vds11@LAPTOP-OPKM5TC1>
2021-05-20 16:43:27 +0900

    회원테이블 작성

---
+++ gooddeal/src/main/webapp/sql/member.sql
@@ -0,0 +1,25 @@
+create table member(
+	mem_num number not null,
+	mem_id varchar2(15) not null,
+	mem_now varchar2(1) not null,
+    constraint member_pk primary key(mem_num)
+);
+
+
+create table member_detail(
+	mem_num number not null,
+	mem_password varchar2(20) not null,
+	mem_name varchar2(15) not null,
+	mem_propick blob,
+	mem_credit number default 5 not null,
+	mem_phone varchar2(13) not null,
+	mem_email varchar2(50) not null,
+	mem_zipcode varchar2(5) not null,
+	mem_address1 varchar2(100) not null,
+	mem_address2 varchar2(100) not null,
+	mem_date date default sysdate,
+	constraint member_detail_pk primary key (mem_num),
+	constraint member_detail_fk foreign key (mem_num) references member (mem_num)
+);
+
+create sequence member_seq;
