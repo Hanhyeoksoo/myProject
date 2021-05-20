0d548d47661132fa6530ec9ed61e088b4dfa17be
user <user@DESKTOP-JGQT69O>
2021-05-20 17:50:45 +0900

    테이블 작성

---
+++ gooddeal/src/main/webapp/sql/cheat.sql
@@ -0,0 +1,13 @@
+/*신고 게시판(비공개)*/
+create table cheat_board(
+    che_num NUMBER not null ,
+    mem_num number not null ,
+    che_title VARCHAR2(300) not null,
+    che_content VARCHAR2(3000)  not null,
+    che_pnum number not null,
+    che_view INT not null,
+    che_admincheck number(1) not null /* 1:미확인 2:확인 */,
+    che_date date not null,
+    constraint cheat_board_pk primary key (che_num),
+	constraint cheat_board_fk foreign key (mem_num) references member (mem_num)
+); 
