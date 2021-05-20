df2b3223961a23c5e440d59014332acecb4d10dd
ezen <ezen@DESKTOP-O03MT84>
2021-05-20 16:53:10 +0900

    event sql 업로드

---
+++ gooddeal/src/main/webapp/sql/event.sql
@@ -0,0 +1,21 @@
+CREATE TABLE event(
+ eve_num number not null,
+ mem_num number not null,
+ eve_title varchar2(300) not null,
+ eve_content clob not null,
+ eve_user varchar2(300) not null,
+ eve_start varchar2(10) not null,
+ eve_end varchar2(10) not null,
+ eve_date date not null, 
+ constraint event_pk primary key (eve_num)
+);
+
+CREATE TABLE event_result(
+ eve_num number not null,
+ mem_num number not null,
+ eve_status number(1) default 1 not null, 
+ constraint event_result_pk primary key (eve_num),
+ constraint event_result_fk foreign key (mem_num) references event (eve_num)
+);
+
+CREATE SEQUENCE event_seq;
