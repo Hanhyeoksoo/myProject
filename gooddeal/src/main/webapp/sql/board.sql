1f3110b9d2833a0a10b6acb1dca5558bfb125c09
rlaeh <rlaeh@LAPTOP-1EISNQHF>
2021-05-20 16:58:14 +0900

    board

---
+++ gooddeal/src/main/webapp/sql/board.sql
@@ -0,0 +1,23 @@
+create table board(
+	boa_num number not null primary key,
+	mem_num number foreign key,
+	boa_title varchar2(150) not null,
+	boa_content clob not null,
+	boa_uploadfile blob,
+	boa_date date not null,
+	boa_update date not null,
+	boa_view number not null	
+);
+
+create table board_reply(
+	bor_num number not null primary key,
+	boa_num number not null foreign key,
+	bor_content varchar2(3000) not null,
+	bor_date blob not null	
+);
+
+create table board_fav(
+	bof_num number not null primary key,
+	boa_num number not null foreign key,
+	mem_num number not null foreign key
+);
