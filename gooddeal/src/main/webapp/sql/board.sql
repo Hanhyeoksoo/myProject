9ea6d5c3d32c648a853798397bfdd37a9b2f839c
tjong <tjong@DESKTOP-ODLME97>
2021-05-27 09:36:08 +0900

    이벤트 페이지

--- gooddeal/src/main/webapp/sql/board.sql
+++ gooddeal/src/main/webapp/sql/board.sql
@@ -1,5 +1,5 @@
 create table board(
-	boa_num number not null primary key,
+	boa_num number not null,
 	mem_num number not null,
 	boa_title varchar2(150) not null,
 	boa_content clob not null,
@@ -7,21 +7,24 @@ create table board(
 	boa_date date not null,
 	boa_update date not null,
 	boa_view number not null,
+	constraint board_pk primary key (boa_num),
     CONSTRAINT board_fk foreign key (mem_num) references member (mem_num)
 );
 
 create table board_reply( 
-	bor_num number not null primary key,
+	bor_num number not null,
 	boa_num number not null,
 	bor_content varchar2(3000) not null,
 	bor_date blob not null,
+	constraint board_pk primary key (bor_num),
     CONSTRAINT board_fk2 foreign key (boa_num) references board (boa_num)
 );
            
 create table board_fav(
-	bof_num number not null primary key,
+	bof_num number not null,
 	boa_num number not null,
 	mem_num number not null,
+	constraint board_pk primary key (bof_num),
     CONSTRAINT board_fk3 foreign key (boa_num) references board (boa_num),
     CONSTRAINT board_fk4 foreign key (mem_num) references member (mem_num)
 );
